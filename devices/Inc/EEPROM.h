853e3cfe036d7e085fd1db48676ab8758a17e8df
Mike <photo.mickey@gmail.com>
2020-08-07 13:48:23 +0300

    Debug condidate

---
+++ devices/Inc/EEPROM.h
@@ -0,0 +1,68 @@
+/*
+ * * * *  EEPROMM01 1-Mbit serial SPI bus EEPROM * * * *
+ * Hight speed clock: 16 MHz
+ * Byte write within: 5 ms
+ * Page write within: 5 ms
+ */
+#ifndef INC_EEPROM_H_
+#define INC_EEPROM_H_
+/*----------------------- Includes -------------------------------------*/
+#include "stm32f2xx_hal.h"
+#include "stm32f2xx_hal_spi.h"
+/*------------------------ Macros --------------------------------------*/
+#define  REVERSE_BYTE( b )   ( ( ( b << 7U ) & 0x80U ) | ( ( b << 5U ) & 0x40U ) | ( ( b << 3U ) & 0x20U ) | ( ( b << 1U ) & 0x10U ) | ( ( b >> 1U ) & 0x08U ) | ( ( b >> 3U ) & 0x04U ) | ( ( b >> 5U ) & 0x02U ) | ( ( b >> 7U ) & 0x01U ) )
+#define  ADR_TO_UINT( a )    ( ( (uint32_t)a[0U] ) | ( ( (uint32_t)a[1U] ) << 8U ) | ( ( (uint32_t)a[2U] ) << 16U ) )
+/*------------------------ Define --------------------------------------*/
+/*---------- Software parameters ------*/
+#define  EEPROM_TIMEOUT           1000U
+/*---------- Chip parameters ----------*/
+#define  EEPROM_PAGE_SIZE         0x100U    /* bytes */
+#define  EEPROM_ADR_LEN           24U       /* bit */
+#define  EEPROM_SIZE              0x200U     /* Kb */
+#define  EEPROM_PAGE_NUM          0x200U    /* pages */
+#define  EEPROM_MAX_ADR           0x1FFFFU
+/*---------- Commands -----------------*/
+#define  EEPROM_WREN              0x06U     /* Write Enable */
+#define  EEPROM_WRDI              0x04U     /* Write Disable */
+#define  EEPROM_RDSR              0x05U     /* Read Status Register */
+#define  EEPROM_WRSR              0x01U     /* Write Status Register */
+#define  EEPROM_READ              0x03U     /* Read from Memory Array */
+#define  EEPROM_WRITE             0x02U     /* Write to Memory Array */
+#define  EEPROM_RDID              0x83U     /* Read Identification Page */
+#define  EEPROM_WRID              0x82U     /* Write Identification Page */
+#define  EEPROM_RDLS              0x83U     /* Reads the Identification Page lock status (only for EEPROMM01-D) */
+#define  EEPROM_LIF               0x82U     /* Locks the Identification Page in read-only mode (only for EEPROMM01-D) */
+/*---------- Status register ----------*/
+#define  EEPROM_SR_WIP            0x01U     /* Write In Progress */
+#define  EEPROM_SR_WEL            0x02U     /* Write Enable Latch */
+#define  EEPROM_SR_BP0            0x04U     /* Block Protect 0 */
+#define  EEPROM_SR_BP1            0x08U     /* Block Protect 1 */
+#define  EEPROM_SR_SRWD           0x80U     /* Status Register Write Disable */
+#define  EEPROM_SR_WPNO           0x00U     /* Write-protected none */
+#define  EEPROM_SR_WPUQ           0x04U     /* Write-protected Upper quarter: 0x18000 - 0x1FFFF */
+#define  EEPROM_SR_WPUH           0x08U     /* Write-protected Upper half: 0x10000 - 0x1FFFF */
+#define  EEPROM_SR_WPWM           0x0CU     /* Write-protected Whole memory: 0x00000 - 0x1FFFF */
+/*------------------------- Enum ---------------------------------------*/
+typedef enum
+{
+  EEPROM_OK,         /* Command done and bus is free for new one */
+  EEPROM_BUSY,       /* Some process in progress */
+  EEPROM_ADR_ERROR,  /* Try to get access over address */
+  EEPROM_INIT_ERROR, /* No SPI structure*/
+  EEPROM_ERROR,      /* Other errors */
+} EEPROM_STATUS;
+
+typedef enum
+{
+  EEPROM_SR_IDLE,        /* Idle state */
+  EEPROM_SR_BUSY,        /* Write process in progress */
+  EEPROM_SR_WRITE_READY, /* Memory is enabled for writing */
+  EEPROM_SR_UNBLOCK,     /* All memory is unprotected for writing BP0 = 0, BP1 = 0 */
+} EEPROM_SR_STATE;
+/*----------------------- Functions ------------------------------------*/
+void          vEEPROMInit( SPI_HandleTypeDef* hspi );                            /* Installation of EEPROM */
+void          vEEPROMformAdr ( uint32_t adr, uint8_t* buffer );                  /* Transferm uint32_t address to the 24bit array ( uint8_t x3 ) */
+EEPROM_STATUS eEEPROMReadMemory ( uint8_t* adr, uint8_t* data, uint32_t len );   /* Read memory of EEPROM */
+EEPROM_STATUS eEEPROMWriteMemory ( uint8_t* adr, uint8_t* data, uint32_t len );  /* Write data to memory of EEPROM */
+/*----------------------------------------------------------------------*/
+#endif /* INC_EEPROM_H_ */
---
+++ devices/Src/EEPROM.c
@@ -0,0 +1,310 @@
+/*--------------------------------- Includes ---------------------------------*/
+#include "EEPROM.h"
+#include "cmsis_os2.h"
+/*-------------------------------- Structures --------------------------------*/
+static SPI_HandleTypeDef* EEPROM_SPI = NULL;
+/*--------------------------------- Constant ---------------------------------*/
+/*-------------------------------- Variables ---------------------------------*/
+/*-------------------------------- Functions ---------------------------------*/
+EEPROM_STATUS eEEPROMwrite( uint8_t cmd, uint8_t* adr, uint8_t* data, uint8_t size );  /* Send data via SPI to the EEPROM */
+EEPROM_STATUS eEEPROMread( uint8_t cmd, uint8_t* adr, uint8_t* data, uint8_t size );   /* Get data via SPI from EEPROM */
+EEPROM_STATUS eEEPROMwriteEnable( void );                                              /* Enable writing */
+EEPROM_STATUS eEEPROMwriteDisable( void );                                             /* Disable writing */
+EEPROM_STATUS eEEPROMreadSR( EEPROM_SR_STATE* status );                                /* Read status regester */
+EEPROM_STATUS eEEPROMwriteSR( uint8_t data );                                          /* Write status register */
+EEPROM_STATUS vEEPROMUnblock( void );                                                  /* Unblock memory for writing */
+EEPROM_STATUS vEEPROMpoolUntil( EEPROM_SR_STATE target );                              /* Waiting for state */
+/*----------------------------------------------------------------------------*/
+/*----------------------- PRIVATE --------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+/*
+ * Send data via SPI to the EEPROM
+ * Input:  cmd  - command of operation
+ *         adr  - addition address of memory 3 bytes ( can be null )
+ *         data - addition data to write ( can be null )
+ *         size - size of addition data ( can be 0U )
+ * Outpur: Status of operation
+ */
+EEPROM_STATUS eEEPROMwrite( uint8_t cmd, uint8_t* adr, uint8_t* data, uint8_t size )
+{
+  HAL_StatusTypeDef hal        = HAL_OK;
+  EEPROM_STATUS     res        = EEPROM_OK;
+  uint8_t           buffer[4U] = { cmd, 0x00U, 0x00U, 0x00U };
+  uint8_t           bufferLen  = 1U;
+  if ( EEPROM_SPI != NULL )
+  {
+    if ( adr != NULL )
+    {
+      buffer[1U] = adr[0U];
+      buffer[2U] = adr[1U];
+      buffer[3U] = adr[2U];
+      bufferLen  = 4U;
+    }
+    while ( EEPROM_SPI->State != HAL_SPI_STATE_READY )
+    {
+      osDelay( 1U );
+    }
+    hal = HAL_SPI_Transmit( EEPROM_SPI, buffer, bufferLen, EEPROM_TIMEOUT );
+    if ( hal == HAL_OK )
+    {
+      if ( size > 0U )
+      {
+        while ( EEPROM_SPI->State != HAL_SPI_STATE_READY )
+        {
+    	  osDelay( 1U );
+        }
+        hal = HAL_SPI_Transmit( EEPROM_SPI, data, size, EEPROM_TIMEOUT );
+        if ( hal != HAL_OK )
+        {
+          res = EEPROM_ERROR;
+        }
+      }
+    }
+    else
+    {
+      res = EEPROM_ERROR;
+    }
+  }
+  else
+  {
+    res = EEPROM_INIT_ERROR;
+  }
+  return res;
+}
+/*
+ * Get data via SPI from EEPRO
+ * Input:  cmd  - command of operation
+ *         adr  - addition address of memory 3 bytes ( can be null )
+ *         data - addition data to write ( can be null )
+ *         size - size of addition data ( can be 0U )
+ * Outpur: Status of operationM
+ */
+EEPROM_STATUS eEEPROMread( uint8_t cmd, uint8_t* adr, uint8_t* data, uint8_t size )
+{
+  HAL_StatusTypeDef hal        = HAL_OK;
+  EEPROM_STATUS     res        = EEPROM_OK;
+  uint8_t           buffer[4U] = { cmd, 0x00U, 0x00U, 0x00U };
+  uint8_t           bufferLen  = 1U;
+  if ( EEPROM_SPI != NULL )
+  {
+	if ( adr != NULL )
+	{
+	  buffer[1U] = adr[0U];
+	  buffer[2U] = adr[1U];
+	  buffer[3U] = adr[2U];
+	  bufferLen  = 4U;
+	}
+    while ( EEPROM_SPI->State != HAL_SPI_STATE_READY )
+    {
+      osDelay( 1U );
+    }
+    hal = HAL_SPI_Transmit( EEPROM_SPI, buffer, bufferLen, EEPROM_TIMEOUT );
+    if ( hal == HAL_OK )
+    {
+      if ( size > 0U )
+      {
+        while ( EEPROM_SPI->State != HAL_SPI_STATE_READY )
+        {
+          osDelay( 1U );
+        }
+        hal = HAL_SPI_Receive( EEPROM_SPI, data, size, EEPROM_TIMEOUT );
+        if ( hal != HAL_OK )
+        {
+          res = EEPROM_ERROR;
+        }
+      }
+    }
+    else
+    {
+      res = EEPROM_ERROR;
+    }
+  }
+  else
+  {
+    res = EEPROM_INIT_ERROR;
+  }
+  return res;
+}
+/*
+ * Enable write operations of EEPROM
+ * Input:  None
+ * Output: Status of operation
+ */
+EEPROM_STATUS eEEPROMwriteEnable( void )
+{
+  return eEEPROMwrite( EEPROM_WREN, NULL, NULL, 0U );
+}
+/*
+ * Disable write operations of EEPROM
+ * Input:  None
+ * Output: Status of operation
+ */
+EEPROM_STATUS eEEPROMwriteDisable( void )
+{
+  return eEEPROMwrite( EEPROM_RDSR, NULL, NULL, 0U );
+}
+/*
+ * Read status of EEPROM
+ * Input:  status - status of EEPROM
+ * Output: Status of operation
+ */
+EEPROM_STATUS eEEPROMreadSR( EEPROM_SR_STATE* status )
+{
+  uint8_t       data = 0x00U;
+  EEPROM_STATUS res  = eEEPROMread( EEPROM_RDSR, NULL, &data, 1U );
+  if ( data & EEPROM_SR_WIP )
+  {
+    *status = EEPROM_SR_BUSY;
+  }
+  else if ( data & EEPROM_SR_WEL )
+  {
+	*status = EEPROM_SR_WRITE_READY;
+  }
+  else if ( ( data & ( EEPROM_SR_BP0 | EEPROM_SR_BP1 ) ) == 0U )
+  {
+    *status = EEPROM_SR_UNBLOCK;
+  }
+  else
+  {
+    *status = EEPROM_SR_IDLE;
+  }
+  return res;
+}
+/*
+ * Write status register of EEPROM
+ * Input:  data - data for writing
+ * Output: Status of operation
+ */
+EEPROM_STATUS eEEPROMwriteSR( uint8_t data )
+{
+  return eEEPROMwrite( EEPROM_RDSR, NULL, &data, 1U );
+}
+/*
+ * Unblock EEPROM memory for writing
+ * Input:  none
+ * Output: Status of operation
+ */
+EEPROM_STATUS vEEPROMunblock ( void )
+{
+  uint8_t       data = 0x00;
+  EEPROM_STATUS res  = eEEPROMread( EEPROM_RDSR, NULL, &data, 1U );
+  if ( res == EEPROM_OK )
+  {
+    data |= EEPROM_SR_BP0 | EEPROM_SR_BP1;
+    res = eEEPROMwrite( EEPROM_WRSR, NULL, &data, 1U );
+  }
+  return res;
+}
+/*
+ * Wait some status of EEPROM
+ * Input:  target - target status of EEPROM
+ * Output: Status of operation
+ */
+EEPROM_STATUS vEEPROMpoolUntil ( EEPROM_SR_STATE target )
+{
+  EEPROM_STATUS   res   = EEPROM_OK;
+  EEPROM_SR_STATE state = EEPROM_SR_IDLE;
+  do
+  {
+    res = eEEPROMreadSR( &state );
+    osDelay( 5U );
+  } while ( ( state != target ) || ( res != EEPROM_ERROR ));
+  return res;
+}
+/*----------------------------------------------------------------------------*/
+/*----------------------- PABLICK --------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+/*
+ * Installation of EEPROM
+ * Input:  hspi - pointer to the spi structure
+ * Output: none
+ */
+void vEEPROMInit( SPI_HandleTypeDef* hspi )
+{
+  EEPROM_SPI = hspi;
+  return;
+}
+/*
+ * Transferm uint32_t address to the 24bit array ( uint8_t x3 )
+ * Input:  adr    - input address
+ *         buffer - output aaray
+ * Output: none
+ */
+void vEEPROMformAdr ( uint32_t adr, uint8_t* buffer )
+{
+  buffer[0U] = ( uint8_t )adr;
+  buffer[1U] = ( uint8_t )( adr >> 8U );
+  buffer[2U] = ( uint8_t )( adr >> 16U );
+  return;
+}
+/*
+ * Read memory of EEPROM
+ * Input:  adr  - address in memory
+ *         data - output read data
+ *         len  - length to read
+ * Output: Status of operation
+ */
+EEPROM_STATUS eEEPROMReadMemory ( uint8_t* adr, uint8_t* data, uint32_t len )
+{
+  EEPROM_STATUS res        = EEPROM_OK;
+  uint32_t      adr32      = ADR_TO_UINT( adr );
+  uint8_t       buffer[4U] = { EEPROM_READ, REVERSE_BYTE( adr[2U] ), REVERSE_BYTE( adr[1U] ), REVERSE_BYTE( adr[0U] ) };
+  if ( adr32 <= EEPROM_MAX_ADR )
+  {
+    res = vEEPROMpoolUntil( EEPROM_SR_IDLE );
+    if (res == EEPROM_OK )
+    {
+      res = eEEPROMread( EEPROM_READ, buffer, data, len );
+    }
+  }
+  else
+  {
+    res = EEPROM_ADR_ERROR;
+  }
+  return res;
+}
+/*
+ * Write data to memory of EEPROM
+ * Input:  adr  - address in memory
+ *         data - data to write
+ *         len  - length to write
+ * Output: Status of operation
+ */
+EEPROM_STATUS eEEPROMWriteMemory ( uint8_t* adr, uint8_t* data, uint32_t len )
+{
+  EEPROM_STATUS res        = EEPROM_OK;
+  uint32_t      adr32      = ADR_TO_UINT( adr );
+  uint8_t       buffer[4U] = { EEPROM_WRITE, REVERSE_BYTE( adr[2U] ), REVERSE_BYTE( adr[1U] ), REVERSE_BYTE( adr[0U] ) };
+  if ( adr32 <= EEPROM_MAX_ADR )
+  {
+    res = vEEPROMpoolUntil( EEPROM_SR_IDLE );
+    if ( res == EEPROM_OK )
+    {
+      res = eEEPROMwriteEnable();
+      if ( res == EEPROM_OK )
+      {
+        res = vEEPROMpoolUntil( EEPROM_SR_WRITE_READY );
+        if ( res == EEPROM_OK )
+        {
+          res = vEEPROMunblock();
+          if ( res == EEPROM_OK )
+          {
+            res = vEEPROMpoolUntil( EEPROM_SR_UNBLOCK );
+            if ( res == EEPROM_OK )
+            {
+              res = eEEPROMwrite( EEPROM_WRITE, buffer, data, len );
+            }
+          }
+        }
+      }
+    }
+  }
+  else
+  {
+    res = EEPROM_ADR_ERROR;
+  }
+  return res;
+}
+/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/

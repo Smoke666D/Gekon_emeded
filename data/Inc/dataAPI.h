5fd482850454da898281b4733233bd05f83b1143
Mike <photo.mickey@gmail.com>
2020-08-27 17:55:06 +0300

    Config API for test

--- data/Inc/dataAPI.h
+++ data/Inc/dataAPI.h
@@ -13,14 +13,33 @@
 #include "EEPROM.h"
 #include "config.h"
 /*------------------------ Define --------------------------------------*/
-
+#define  SEMAPHORE_TAKE_DELAY  ( ( TickType_t ) 10U )
 /*------------------------- Macros -------------------------------------*/
 
 /*-------------------------- ENUM --------------------------------------*/
+typedef enum
+{
+  DATA_API_CMD_WRITE,    /* Save data from external data buffer to the data register */
+  DATA_API_CMD_INC,      /* Increment data in the register, the external data buffer is ignored */
+  DATA_API_CMD_DEC,      /* Decrement data in the register, the external data buffer is ignored */
+  DATA_API_CMD_ESC,      /* Reset changes of data: write storage data from the register to the external data buffer */
+  DATA_API_CMD_SAVE,     /* Save all data registers to the EEPROM. The input data is ignore */
+  DATA_API_CMD_LOAD,     /* Load all data registers from the EEPROM. The input data is ignore */
+} DATA_API_COMMAND;
 
+typedef enum
+{
+  DATA_API_STAT_OK,            /* All done correct */
+  DATA_API_STAT_BUSY,          /* The API is busy with another operation */
+  DATA_API_STAT_MIN_ERROR,     /* Data is too small */
+  DATA_API_STAT_MAX_ERROR,     /* Data is too big */
+  DATA_API_STAT_EEPROM_ERROR,  /* Error during EEPROM write operation */
+  DATA_API_STAT_ERROR,         /* Other error */
+} DATA_API_STATUS;
 /*----------------------- Structures -----------------------------------*/
 
 /*------------------------ Functions -----------------------------------*/
-
+void            vDATAAPIinit ( void );
+DATA_API_STATUS eDATAAPIconfigValue ( DATA_API_COMMAND cmd, uint16_t adr, uint16_t* data );
 /*----------------------------------------------------------------------*/
 #endif /* INC_DATAAPI_H_ */
--- data/Src/dataAPI.c
+++ data/Src/dataAPI.c
@@ -5,9 +5,15 @@
  *      Author: 79110
  */
 /*----------------------- Includes ------------------------------------------------------------------*/
+#include "dataAPI.h"
+#include "cmsis_os.h"
+#include "FreeRTOS.h"
+#include "semphr.h"
 #include "storage.h"
 #include "chart.h"
+#include "storage.h"
 /*----------------------- Structures ----------------------------------------------------------------*/
+static SemaphoreHandle_t xSemaphore;
 /*----------------------- Constant ------------------------------------------------------------------*/
 /*----------------------- Variables -----------------------------------------------------------------*/
 /*----------------------- Functions -----------------------------------------------------------------*/
@@ -19,7 +25,100 @@
 /*---------------------------------------------------------------------------------------------------*/
 /*----------------------- PABLICK -------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------------------------------*/
+void vDATAAPIinit ( void )
+{
+  xSemaphore = xSemaphoreCreateMutex();
+  return;
+}
+/*---------------------------------------------------------------------------------------------------*/
+/*
+ * Write API for configurations
+ * input:  cmd  - command for operation
+ *         adr  - address of configuration in configReg array
+ *         data - pointer to the external buffer. The length of buffer is in attribute of configuration
+ * output: status of operation
+ */
+DATA_API_STATUS eDATAAPIconfigValue ( DATA_API_COMMAND cmd, uint16_t adr, uint16_t* data )
+{
+  DATA_API_STATUS res = DATA_API_STAT_OK;
+  uint8_t         i   = 0U;
 
+  if ( xSemaphore != NULL )
+  {
+    if ( xSemaphoreTake( xSemaphore, SEMAPHORE_TAKE_DELAY ) == pdTRUE )
+    {
+      switch ( cmd )
+      {
+        case DATA_API_CMD_WRITE:
+          if ( configReg[adr]->atrib->max != 0U )
+          {
+            if ( data[0U] > configReg[adr]->atrib->max )
+            {
+              res = DATA_API_STAT_MAX_ERROR;
+            }
+            if ( data[0U] < configReg[adr]->atrib->min )
+            {
+              res = DATA_API_STAT_MIN_ERROR;
+            }
+          }
+          if ( res == DATA_API_STAT_OK )
+          {
+            for ( i=0; i<configReg[adr]->atrib->len; i++ )
+            {
+              configReg[adr]->value[i] = data[i];
+            }
+          }
+          break;
+        case DATA_API_CMD_SAVE:
+          if ( eSTORAGEwriteConfigs() != EEPROM_OK )
+          {
+            res = DATA_API_STAT_EEPROM_ERROR;
+          }
+          break;
+        case DATA_API_CMD_ESC:
+          for ( i=0; i<configReg[adr]->atrib->len; i++ )
+          {
+            data[i] = configReg[adr]->value[i];
+          }
+          break;
+        case DATA_API_CMD_INC:
+          if ( ( configReg[adr]->atrib->max != 0U ) && ( configReg[adr]->value[0U] < configReg[adr]->atrib->max ) )
+          {
+            configReg[adr]->value[0U]++;
+          }
+          else
+          {
+            res = DATA_API_STAT_MAX_ERROR;
+          }
+          break;
+        case DATA_API_CMD_LOAD:
+          if ( eSTORAGEreadConfigs() != EEPROM_OK )
+          {
+            data[i] = configReg[adr]->value[i];
+          }
+          break;
+        case DATA_API_CMD_DEC:
+          if ( ( configReg[adr]->atrib->max != 0U ) && ( configReg[adr]->value[0U] > configReg[adr]->atrib->min ) )
+          {
+	    configReg[adr]->value[0U]--;
+          }
+          else
+          {
+            res = DATA_API_STAT_MAX_ERROR;
+          }
+          break;
+        default:
+          break;
+      }
+      xSemaphoreGive( xSemaphore );
+    }
+    else
+    {
+      res = DATA_API_STAT_BUSY;
+    }
+  }
+  return res;
+}
 /*---------------------------------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------------------------------*/

771abbfbaa334b0877730f2f42ab5144dff949e6
Mike <photo.mickey@gmail.com>
2021-02-09 19:25:18 +0300

    Turn off display on cranking

--- dieselController/Src/engine.c
+++ dieselController/Src/engine.c
@@ -21,6 +21,7 @@
 #include "stdio.h"
 #include "alarm.h"
 #include "debug.h"
+#include "lcd.h"
 /*-------------------------------- Structures --------------------------------*/
 static ENGINE_TYPE           engine              = { 0U };
 static OIL_TYPE              oil                 = { 0U };
@@ -1517,6 +1518,7 @@ void vENGINEtask ( void* argument )
               //charger.hightPreAlarm.error.active = PERMISSION_DISABLE;
               engine.status                      = ENGINE_STATUS_BUSY_STARTING;
               vELECTROsendCmd( ELECTRO_CMD_DISABLE_START_ALARMS );
+              vLCD_BrigthOFF();
               vRELAYset( &fuel.pump, RELAY_ON );
               if ( starter.idlingDelay != 0U )
               {
@@ -1547,6 +1549,7 @@ void vENGINEtask ( void* argument )
                 starter.set( RELAY_OFF );
                 starterFinish  = 1U;
                 starter.status = STARTER_CONTROL_BLOCK;
+                vLCD_BrigthOn();
                 vLOGICresetTimer( &commonTimer );
                 commonTimer.delay = starter.blockDelay;
                 vLOGICstartTimer( &commonTimer );

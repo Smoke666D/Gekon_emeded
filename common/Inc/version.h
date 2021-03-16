eb5a607af3eff6ed913662150f5a48cd2393b361
Mike <photo.mickey@gmail.com>
2021-03-16 16:22:47 +0300

    Update version

--- common/Inc/version.h
+++ common/Inc/version.h
@@ -8,7 +8,7 @@
 #ifndef INC_VERSION_H_
 #define INC_VERSION_H_
 /*------------------------ Define --------------------------------------*/
-#define SOFTWARE_VERSION	00001U
+#define SOFTWARE_VERSION	00004U
 #define HARDWARE_VERSION	00002U
 /*----------------------------------------------------------------------*/
 #endif /* INC_VERSION_H_ */
